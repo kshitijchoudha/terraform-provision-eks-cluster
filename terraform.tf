4677edbb57c77315a42820aaa4ca8d056dd26142
KC <kchoudha@gmail.com>
2022-12-11 18:47:00 -0600

    add tfc provider

--- terraform.tf
+++ terraform.tf
@@ -25,6 +25,12 @@ terraform {
       version = "~> 2.12.1"
     }
   }
+  cloud {
+    organization = "example-org-7e8de7"
+
+    workspaces {
+      name = "eks-deploymento"
+
 
   required_version = "~> 1.3"
 }
